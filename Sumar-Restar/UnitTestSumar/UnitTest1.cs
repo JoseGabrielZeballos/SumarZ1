58ada6b78256a2750b56539646676e70dd3a64cf
JoseGabrielZeballos <a2019117789@estudiantes.upsa.edu.bo>
2020-08-15 11:56:31 -0400

    Segundo unitTest con negativos

--- Sumar-Restar/UnitTestSumar/UnitTest1.cs
+++ Sumar-Restar/UnitTestSumar/UnitTest1.cs
@@ -21,5 +21,21 @@ namespace UnitTestSumar
             Assert.AreEqual(esperado, resultaso);
         }
 
+        [TestMethod]
+        public void TestAdd2()
+        {
+            //Arrange
+            Sumacontroller sumita = new Sumacontroller();
+            int a = -7;
+            int b = -6;
+            int esperado = -13;
+
+            //ACT
+            int resultaso = sumita.Add(a, b);
+            //Assert
+            Assert.AreEqual(esperado, resultaso);
+        }
+
+
     }
 }
