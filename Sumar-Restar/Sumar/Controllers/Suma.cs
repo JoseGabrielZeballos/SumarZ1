05cc9f9707f474faea952ffdc1a830ce1404fe72
JoseGabrielZeballos <a2019117789@estudiantes.upsa.edu.bo>
2020-08-15 09:50:47 -0400

    Suma y multiplicacion.

--- Sumar-Restar/Sumar/Controllers/Suma.cs
+++ Sumar-Restar/Sumar/Controllers/Suma.cs
@@ -12,14 +12,28 @@ namespace Suma.Controllers
     public class Sumacontroller : ControllerBase
     {
         [HttpGet]
+        [Route("Add")]
         public int Add(int a, int b)
         {
             return a + b;
         }
         [HttpPost]
+        [Route("Add")]
         public int Add2([FromHeader] int a, [FromHeader] int b)
         {
             return a + b;
+        }
+        [HttpGet]
+        [Route("Multiply")]
+        public int MUL1(int a, int b)
+        {
+            return a * b;
+        }
+        [HttpPost]
+        [Route("Multiply")]
+        public int MUL2([FromHeader] int a, [FromHeader] int b)
+        {
+            return a * b;
         }
     }
 }
